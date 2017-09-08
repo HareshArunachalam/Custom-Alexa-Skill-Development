edbb2938bd1169d1e7dcd0645c4d5ff0095567c3
HareshArunachalam <31510135+HareshArunachalam@users.noreply.github.com>
2017-09-08 19:10:18 +0530

    Add files via upload

---
+++ 7 Welcome_Skill_Raspberry_Pi_https_Server/welcome.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_ask import Ask, statement, convert_errors, question
+import logging
+
+app = Flask(__name__)
+
+ask = Ask(app, '/')
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@ask.launch
+def LaunchRequest():
+        speech_text = "You can ask Maker to welcome"
+        card_title = "Welcome Skill"
+        return question(speech_text).simple_card(card_title, speech_text).reprompt(speech_text)       $
+
+@ask.intent('WelcomeIntent')
+def welcomeIntent():
+        return statement("Welcome to the course, hope you enjoy learning and making with us").simple_c$
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
